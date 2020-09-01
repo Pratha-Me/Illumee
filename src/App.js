2603b4d62ca861ceb449775337f892184290b947
Pramosh Shrestha <pratha.mero@pm.me>
2020-09-02 01:23:14 +0545

    Body is initialised

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import './styles/style.css';
 import { Router, Switch, Route } from 'react-router-dom';
 import history from './services/history';
 import Home from './components/Home';
+
 function App() {
   return (
     <div className="App">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react'
+import dashboard from '../images/Dashboard.png';
+
 export default class Home extends Component {
     render() {
         return (
-            <div className="intro">
-                <h2 className="brand">IILU<span>ME</span>E</h2>
-                <h1 className="heading"> COMING SOON </h1>
+            <React.Fragment>
+                <div className="intro-heading">
+                    <h2 className="brand">IILU<span>ME</span>E</h2>
+                    <h1 className="heading"> COMING SOON </h1>
 
+                    <p>GET NOTIFIED WHEN IT'S READY</p>
+                    <div className="input-div">
+                        <input type="email" name="email" placeholder="Enter your email"></input><button> Notify me</button>
+                    </div>
+                    <p className="last-p">INVESTORS INFORMATION</p>
 
-                <p>GET NOTIFIED WHEN IT'S READY</p>
-                <input type="email" name="email" placeholder="Enter your email"></input><button> Notify me</button>
-                <p className="last-p">INVESTORS INFORMATION</p>
-            </div>
+                </div>
+
+                <div className="illumee-body">
+                    <h1 className="body-heading"> What is ILLUMEE?</h1>
+                    <div className="flex-div">
+                        <img className="image-resize" src={dashboard} alt="Dashboard"></img>
+                        <p className="flex-p">
+                            ILLUMEE is a platform that allows guests to view upcoming talks and search for discussions in specific industries
+                            that the guest may be interested in learning.
+                            <br></br> <br></br>
+                            ILLUMEE is a platform for all experts in different industries. Attendees have access to a wide variety of Live
+                            lectures in any industry they can subscribe to, and ILLUMEE brings the experts to guests remotely.
+                        </p>
+
+                        {/* <div className="img-div">
+                            <img className="img-resize"></img>
+                        </div>
+                        <div className="div-p">
+                            <p className="flex-p"> 
+                            ILLUMEE is a platform that allows guests to view upcoming talks and search for discussions in specific industries
+                            that the guest may be interested in learning.
+                            <br></br>
+                            ILLUMEE is a platform for all experts in different industries. Attendees have access to a wide variety of Live
+                            lectures in any industry they can subscribe to, and ILLUMEE brings the experts to guests remotely.</p>
+                        </div> */}
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
