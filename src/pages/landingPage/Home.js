e9e34a1e611674b855e25e28bc7c3665d2cb0c39
Pramosh Shrestha <pratha.mero@pm.me>
2020-09-02 21:13:02 +0545

    The images were completed and refactored for positions

--- src/pages/landingPage/Home.js
+++ src/pages/landingPage/Home.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import dashboard from '../../images/Dashboard.png';
 import conference from '../../images/Conference.png';
+import store from '../../images/Live conference- store layout 2 – 1.png';
 import img2 from '../../images/Image2.png';
 import img3 from '../../images/Image3.png';
+import img from '../../images/Image.png';
+import image from '../../images/Imagee.png';
 import icon1 from '../../images/Icon awesome-calendar-alt.png';
 import icon2 from '../../images/Icon awesome-city.png';
 import icon3 from '../../images/Icon awesome-plane-departure.png';
@@ -20,18 +23,22 @@ export default class Home extends Component {
             greyComp: {
                 image1: dashboard,
                 alt1: "Dashboard",
-                text1: "ILLUMEE is a platform that allows guests to view upcoming talks and search for discussions in specific industries that the guest may be interested in learning.\n\n ILLUMEE is a platform for all experts in different industries. Attendees have access to a wide variety of Live lectures in any industry they can subscribe to, and ILLUMEE brings the experts to guests remotely.",
+                text1: `ILLUMEE is a platform that allows guests to view upcoming talks and search for discussions in specific industries that the guest may be interested in learning. 
+                <br></br>
+                ILLUMEE is a platform for all experts in different industries. Attendees have access to a wide variety of Live lectures in any industry they can subscribe to, and ILLUMEE brings the experts to guests remotely.`,
 
-                image2: img2,
+                image2: image,
                 alt2: "Quotation",
-                text2: "\"Courage starts with showing up and letting ourselves be seen.\" -- Brene Brown <br></br> <br></br> As the conference devolves we provide our user with a dynamic live chat, where the speaker may interact with the audience, we encourage ou user to connect and share knowledge and expand their network."
+                text2: `\"Courage starts with showing up and letting ourselves be seen.\" -- Brene Brown
+                <br></br>
+                As the conference devolves we provide our user with a dynamic live chat, where the speaker may interact with the audience, we encourage ou user to connect and share knowledge and expand their network.`
             },
             redRelativeComp: {
                 image1: conference,
                 alt1: "Conference",
                 text1: "With ILLUMEE, experts don’t require any specialized tech knowledge to create Live lectures; they don’t have to hire tech gurus to do that either. ILLUMEE makes it easy for them to develop and schedule interactive live conferences/lectures from their home or office’s comfort",
                 
-                image2: conference,
+                image2: store,
                 alt2: "Conference",
                 text2: "As a speaker, we know the importance of leaving the audience with knowledge and perception. With Illumee, we not only provide a location for your lecture/conferences; we also provide you with a storefront. Guests will be able to access the live storefront during the lecture/conference. The lecturer will be able to sell products that live during the talk. Purchase books, products, services being sold during the conference with no lineups or waiting until the end of the event to sell the product. Hosts can now host a live event, and sell products live online during the event."
             }
@@ -60,18 +67,18 @@ export default class Home extends Component {
                     <RedRelativeComp image={this.state.redRelativeComp.image1} alt= {this.state.redRelativeComp.alt1} text={this.state.redRelativeComp.text1}/>
 
                     <div className="flex-img">
-                        <img src={conference} alt="image1" />
                         <img src={img2} alt="image2" />
                         <img src={img3} alt="image3" />
+                        <img src={img} alt="image1" />
                     </div>
 
                     <div className="red-bg-div">
                         {/* <img src={img2} alt="Dashboard"></img> */}
                         <div className="grid-container">
-                            <img className="img-a" src= {icon1} alt="icon1"></img>
-                            <img className="img-b" src= {icon2} alt="icon2"></img>
-                            <img className="img-c" src= {icon3} alt="icon3"></img>
-                            <img className="img-d" src= {icon4} alt="icon4"></img>
+                            <img className="img-a" src= {icon4} alt="icon1"></img>
+                            <img className="img-b" src= {icon3} alt="icon2"></img>
+                            <img className="img-c" src= {icon2} alt="icon3"></img>
+                            <img className="img-d" src= {icon1} alt="icon4"></img>
                         </div>
 
                         <p className="body-p">
