8561beb3550eb0ab8a9cd5c01fd6a809f661bac7
Pramosh Shrestha <pratha.mero@pm.me>
2020-09-08 00:00:11 +0545

    Refactored

--- src/pages/landingPage/RedRelativeComp.js
+++ src/pages/landingPage/RedRelativeComp.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class RedRelativeComp extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className="relative-div">
-                <div className="red-bg valign">
-                    <p className="body-p">
-                        {this.props.text}
-                    </p>
-                </div>
-                <div className="valign abs-div">
-                    <img src={this.props.image} alt={this.props.alt} />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="relative-div">
+        <div className="red-bg valign">
+          <p className="body-p">{this.props.text}</p>
+        </div>
+        <div className="valign abs-div">
+          <img src={this.props.image} alt={this.props.alt} />
+        </div>
+      </div>
+    );
+  }
 }
